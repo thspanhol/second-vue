064be6b5a9ba3ea465b6214b8b0abb422ec1c457
Thales Spanhol <thales_spanhol@hotmail.com>
2023-05-25 21:04:23 -0300

    usando data no componente

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -1,9 +1,16 @@
 <template>
-    <h1>Olá Vue!</h1>
+  <h1>Olá Vue!</h1>
+  <p>Meu nome é {{ nome }} e sou {{ profissao }}.</p>
 </template>
 
 <script>
 export default {
-    name: 'PrimeiroComponente'
+  name: 'PrimeiroComponente',
+  data() {
+    return {
+      nome: 'Thales',
+      profissao: 'programador'
+    }
+  }
 }
 </script>
