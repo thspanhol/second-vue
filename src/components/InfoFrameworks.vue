aab50be004f690392fb5f9bef0616724147f41f7
Thales Spanhol <thales_spanhol@hotmail.com>
2023-06-02 22:30:02 -0300

    metodos

--- src/components/InfoFrameworks.vue
+++ src/components/InfoFrameworks.vue
@@ -4,6 +4,8 @@
     <h3>{{ primeiro }}</h3>
     <h3>{{ segundo }}</h3>
     <h2><a v-bind:href="meu_link" target="_blank">Meu GitHub</a></h2>
+    <button @click="showEmail">{{ texto_email }}</button>
+    <h2 v-show="mostrar_email">{{ email }}</h2>
   </template>
   
   <script>
@@ -15,6 +17,15 @@
         segundo: 'Vue',
         trabalhando: true,
         meu_link: 'https://github.com/thspanhol',
+        email: 'thales_spanhol@hotmail.com',
+        mostrar_email: false,
+        texto_email: 'Mostrar Email',
+      }
+    },
+    methods: {
+      showEmail() {
+        this.mostrar_email = !this.mostrar_email;
+        this.texto_email === 'Mostrar Email' ? this.texto_email = 'Esconder Email' : this.texto_email = 'Mostrar Email';
       }
     }
   }
