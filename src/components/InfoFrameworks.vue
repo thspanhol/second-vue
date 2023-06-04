2fabdbc316a6a6648ac5ff1bb3082c86521268ff
Thales Spanhol <thales_spanhol@hotmail.com>
2023-06-03 21:43:33 -0300

    clean

--- src/components/InfoFrameworks.vue
+++ src/components/InfoFrameworks.vue
@@ -1,32 +1,34 @@
 <template>
-    <h1 v-if="trabalhando">Trabalho com os Seguintes Frameworks:</h1>
-    <h1 v-else>Estudo os seguintes Frameworks:</h1>
-    <h3>{{ primeiro }}</h3>
-    <h3>{{ segundo }}</h3>
-    <h2><a v-bind:href="meu_link" target="_blank">Meu GitHub</a></h2>
-    <button @click="showEmail">{{ texto_email }}</button>
-    <h2 v-show="mostrar_email">{{ email }}</h2>
-  </template>
-  
-  <script>
-  export default {
-    name: 'InfoFrameworks',
-    data() {
-      return {
-        primeiro: 'React',
-        segundo: 'Vue',
-        trabalhando: true,
-        meu_link: 'https://github.com/thspanhol',
-        email: 'thales_spanhol@hotmail.com',
-        mostrar_email: false,
-        texto_email: 'Mostrar Email',
-      }
-    },
-    methods: {
-      showEmail() {
-        this.mostrar_email = !this.mostrar_email;
-        this.texto_email === 'Mostrar Email' ? this.texto_email = 'Esconder Email' : this.texto_email = 'Mostrar Email';
-      }
+  <h1 v-if="trabalhando">Trabalho com os Seguintes Frameworks:</h1>
+  <h1 v-else>Estudo os seguintes Frameworks:</h1>
+  <h3>{{ primeiro }}</h3>
+  <h3>{{ segundo }}</h3>
+  <h2><a v-bind:href="meu_link" target="_blank">Meu GitHub</a></h2>
+  <button @click="showEmail">{{ texto_email }}</button>
+  <h2 v-show="mostrar_email">{{ email }}</h2>
+</template>
+
+<script>
+export default {
+  name: 'InfoFrameworks',
+  data() {
+    return {
+      primeiro: 'React',
+      segundo: 'Vue',
+      trabalhando: true,
+      meu_link: 'https://github.com/thspanhol',
+      email: 'thales_spanhol@hotmail.com',
+      mostrar_email: false,
+      texto_email: 'Mostrar Email'
+    }
+  },
+  methods: {
+    showEmail() {
+      this.mostrar_email = !this.mostrar_email
+      this.texto_email === 'Mostrar Email'
+        ? (this.texto_email = 'Esconder Email')
+        : (this.texto_email = 'Mostrar Email')
     }
   }
-  </script>
+}
+</script>
--- src/components/LifeCycle.vue
+++ src/components/LifeCycle.vue
@@ -1,32 +1,32 @@
 <template>
-    <div>
-       <h1>Meu nome é: {{ nome }}</h1> 
-       <InfoFrameworks />
-    </div>
+  <div>
+    <h1>Meu nome é: {{ nome }}</h1>
+    <InfoFrameworks />
+  </div>
 </template>
 
 <script>
-import InfoFrameworks from './InfoFrameworks.vue';
+import InfoFrameworks from './InfoFrameworks.vue'
 
 export default {
-    name: 'LifeCycle',
-    data() {
-        return {
-            nome: 'Ainda não sei'
-        }
-    },
-    created() {
-        setTimeout(() => {
-            this.nome = 'Thales'
-        }, 1000);
-    },
-    mounted() {
-        setTimeout(() => {
-            this.nome = 'Spanhol'
-        }, 2000);
-    },
-    components: {
-    InfoFrameworks,
+  name: 'LifeCycle',
+  data() {
+    return {
+      nome: 'Ainda não sei'
+    }
   },
+  created() {
+    setTimeout(() => {
+      this.nome = 'Thales'
+    }, 1000)
+  },
+  mounted() {
+    setTimeout(() => {
+      this.nome = 'Spanhol'
+    }, 2000)
+  },
+  components: {
+    InfoFrameworks
+  }
 }
 </script>
