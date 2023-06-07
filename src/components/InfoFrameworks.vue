c96e05b10f55747d2130500fa2bb696924b406c9
Thales Spanhol <thales_spanhol@hotmail.com>
2023-06-06 22:25:54 -0300

    renderizando listas

--- src/components/InfoFrameworks.vue
+++ src/components/InfoFrameworks.vue
@@ -3,6 +3,14 @@
   <h1 v-else>Estudo os seguintes Frameworks:</h1>
   <h3>{{ primeiro }}</h3>
   <h3>{{ segundo }}</h3>
+  <h1>Renderizando lista com v-for: (Array de Strings)</h1>
+  <ul>
+    <li v-for="(item, index) in array" v-bind:key="index">{{ item }}</li>
+  </ul>
+  <h1>Renderizando lista com v-for: (Array de Objetos)</h1>
+  <ul>
+    <li v-for="numeros in objetos" :key="numeros.id">{{ numeros.item }}</li>
+  </ul>
   <h2><a v-bind:href="meu_link" target="_blank">Meu GitHub</a></h2>
   <button @click="showEmail">{{ texto_email }}</button>
   <h2 v-show="mostrar_email">{{ email }}</h2>
@@ -19,7 +27,9 @@ export default {
       meu_link: 'https://github.com/thspanhol',
       email: 'thales_spanhol@hotmail.com',
       mostrar_email: false,
-      texto_email: 'Mostrar Email'
+      texto_email: 'Mostrar Email',
+      array: ['Primeiro Item', 'Segundo Item', 'Terceiro Item'],
+      objetos: [{id: 1, item: 'Um'}, {id: 2, item: 'Dois'}, {id: 3, item: 'Três'}]
     }
   },
   methods: {
