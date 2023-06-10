dd35529b41ff71d94a49aed538c2f48ca897c1b0
Thales Spanhol <thales_spanhol@hotmail.com>
2023-06-09 22:04:00 -0300

    utilizando props

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,26 @@
   <h1>Olá Mundo!</h1>
   <PrimeiroComponente />
   <LifeCycle />
+  <UsaProps recebeProps="Usei Props direta" />
+  <UsaProps :recebeProps="PropsData" />
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
 import LifeCycle from './components/LifeCycle.vue'
+import UsaProps from './components/UsaProps.vue';
 
 export default {
   name: 'App',
   components: {
     PrimeiroComponente,
-    LifeCycle
+    LifeCycle,
+    UsaProps,
+  },
+  data() {
+    return {
+      PropsData: 'Passei o data como Props',
+    }
   }
 }
 </script>
