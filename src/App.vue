7d0b6c67277b2e58a2985d08b9476aa0e75303e8
Thales Spanhol <thales_spanhol@hotmail.com>
2023-06-12 14:39:51 -0300

    emit

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,33 @@
   <LifeCycle />
   <UsaProps recebeProps="Usei Props direta" />
   <UsaProps :recebeProps="PropsData" />
+  <h1>{{ counter }}</h1>
+  <AlteraContador @aumentar-contador="aumentarContador" />
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
 import LifeCycle from './components/LifeCycle.vue'
 import UsaProps from './components/UsaProps.vue'
+import AlteraContador from './components/AlteraContador.vue'
 
 export default {
   name: 'App',
   components: {
     PrimeiroComponente,
     LifeCycle,
-    UsaProps
+    UsaProps,
+    AlteraContador
   },
   data() {
     return {
-      PropsData: 'Passei o data como Props'
+      PropsData: 'Passei o data como Props',
+      counter: 0
+    }
+  },
+  methods: {
+    aumentarContador() {
+      this.counter = this.counter +1
     }
   }
 }
---
+++ src/components/AlteraContador.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <button @click="$emit('aumentarContador')">+1</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AlteraContador',
+    emits: ['aumentarContador']
+}
+</script>
