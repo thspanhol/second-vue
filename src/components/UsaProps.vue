14f2c8f8caa8ad596e2cd127444fd3c60985579e
Thales Spanhol <thales_spanhol@hotmail.com>
2023-06-11 22:03:20 -0300

    clean

--- src/components/UsaProps.vue
+++ src/components/UsaProps.vue
@@ -1,15 +1,14 @@
 <template>
-    <div>
-      <h1>{{ recebeProps }}</h1>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'UsaProps',
-    props: {
-        recebeProps: String
-    },
+  <div>
+    <h1>{{ recebeProps }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UsaProps',
+  props: {
+    recebeProps: String
   }
-  </script>
-  
+}
+</script>
